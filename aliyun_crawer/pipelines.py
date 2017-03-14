4006178ebf403fd0384806d060e37ae49efbb240
xudashuai <xudashuai0819@outlook.com>
2017-03-14 13:34:47 +0800

    mysql

--- aliyun_crawer/pipelines.py
+++ aliyun_crawer/pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import psycopg2,pymysql
+import pymysql
 from aliyun_crawer.settings import HOST
 
 class AliyunCrawerPipeline(object):
