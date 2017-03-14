4d0f6d13c2e195001bbc6aa1044d087dbe16cff1
xudashuai <xudashuai0819@outlook.com>
2017-03-14 14:09:08 +0800

    m

--- aliyun_crawer/pipelines.py
+++ aliyun_crawer/pipelines.py
@@ -11,8 +11,11 @@ class AliyunCrawerPipeline(object):
 
     conn = pymysql.connect(host=HOST,port=3306,user='root',password='19980819',database='data',charset='utf8')
     def process_item(self, item, spider):
-        self.conn.cursor().execute('insert ignore into pois values(%s,%s,%s,%s,%s,%s,%s,%s)',
-                                   (item['id'],item['name'],item['district'],item['type'],item['location'],
-                          item['address'],item['poiweight'],item['businessarea']))
-        self.conn.commit()
+        try:
+            self.conn.cursor().execute('insert ignore into pois values(%s,%s,%s,%s,%s,%s,%s,%s)',
+                                       (item['id'],item['name'],item['district'],item['type'],item['location'],
+                              item['address'],item['poiweight'],item['businessarea']))
+            self.conn.commit()
+        except:
+            pass
         return item
